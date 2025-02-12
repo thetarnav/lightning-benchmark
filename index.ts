c2278bc7968469fc95be6a7ecf084abbc490fa1d
Damian Tarnawski <gthetarnav@gmail.com>
2025-02-12 17:50:53 +0100

    Refactor logging to use dedicated log functions

--- index.ts
+++ index.ts
@@ -11,15 +11,36 @@ const CPU_THROTTLING = 20
 
 
 function force_gc(page: pw.Page): Promise<void> {
-    return page.evaluate("window.gc({type:'major',execution:'sync',flavor:'last-resort'})")
+    return page.evaluate('window.gc({type: "major", execution: "sync", flavor: "last-resort"})')
 }
 
 function set_cpu_throttling(client: pw.CDPSession, rate: number) {
-    return client.send("Emulation.setCPUThrottlingRate", {rate})
+    return client.send('Emulation.setCPUThrottlingRate', {rate})
 }
 
 function sleep(timeout?: number): Promise<void> {
     return new Promise(r => setTimeout(r, timeout))
+}
+
+const ANSI_RESET   = '\x1b[0m'
+const ANSI_RED     = '\x1b[31m'
+const ANSI_GREEN   = '\x1b[32m'
+const ANSI_YELLOW  = '\x1b[33m'
+const ANSI_BLUE    = '\x1b[34m'
+const ANSI_MAGENTA = '\x1b[35m'
+const ANSI_CYAN    = '\x1b[36m'
+const ANSI_GRAY    = '\x1b[90m'
+
+let _log_max_topic_len = 4
+function log(topic: string, msg: string, ...args: any[]): void {
+    _log_max_topic_len = Math.max(_log_max_topic_len, topic.length)
+    let padding = ' '.repeat(_log_max_topic_len - topic.length)
+    console.log(`${ANSI_GRAY}[${topic}${padding}]: ${ANSI_RESET}${msg}`, ...args)
+}
+function error(topic: string, msg: string, ...args: any[]): void {
+    _log_max_topic_len = Math.max(_log_max_topic_len, topic.length)
+    let padding = ' '.repeat(_log_max_topic_len - topic.length)
+    console.error(`${ANSI_RED}[${topic}${padding}]: ${ANSI_RESET}${msg}`, ...args)
 }
 
 function serve() {
@@ -32,7 +53,7 @@ function serve() {
         if (url.pathname === '/') {
             let before = performance.now()
             await build_lightning_solid.build()
-            console.log(`Built in ${(performance.now()-before).toFixed()}ms`)
+            log('SERVE', `Built in ${(performance.now()-before).toFixed()}ms`)
         }
 
         // Handle / -> /index.html
@@ -59,16 +80,16 @@ function serve() {
 
             try {
                 let res = await handle_request(req)
-                console.log('SERVER:', req.method, url.pathname, res.status)
+                log('SERVE', '%s %s %n', req.method, url.pathname, res.status)
                 return res
             } catch (e) {
-                console.error('SERVER:', req.method, url.pathname, e)
+                error('SERVE', '%s %s %n\n%o', req.method, url.pathname, 500, e)
                 return new Response('Server Error', {status: 500})
             }
         },
     })
 
-    console.log(`Server started on ${server.url}`)
+    log('SERVE', `Server started on ${server.url}`)
 
     return server
 }
@@ -81,55 +102,57 @@ async function main() {
         channel:  'chrome',
         headless: false,
         args:     [
-            "--window-size=1000,800",
-            "--js-flags=--expose-gc",
-            "--enable-benchmarking",
-            // "--enable-experimental-web-platform-features",
+            '--window-size=1000,800',
+            '--js-flags=--expose-gc',
+            '--enable-benchmarking',
+            // '--enable-experimental-web-platform-features',
         ],
     })
     
     // Page
     const page = await browser.newPage()
-    console.log('Page created')
+    log('BENCH', 'Page created')
 
     // Console messages
-    page.on("console", msg => {
+    page.on('console', msg => {
         for (let arg of msg.args()) {
-            console.log(`BROWSER: ${arg}`)
+            log('PAGE', arg.toString())
         }
     })
     
     // CDPSession
     const client = await page.context().newCDPSession(page)
-    console.log('CDP session created')
+    log('BENCH', 'CDP session created')
 
     // Website
     await page.goto(server.url.toString(), {waitUntil: 'networkidle'})
-    console.log(`Website ${server.url} loaded`)
+    log('BENCH', `Website ${server.url} loaded`)
 
     // CPU Throttling
     await force_gc(page)
     await set_cpu_throttling(client, CPU_THROTTLING)
-    console.log(`CPU Throttling ${CPU_THROTTLING} enabled`)
+    log('BENCH', `CPU Throttling ${CPU_THROTTLING} enabled`)
 
     // Start Benchmark
     await browser.startTracing(page, {
         path:        './output/trace.json',
         screenshots: false,
         categories:  [
-            "blink.user_timing",
-            "devtools.timeline",
-            "disabled-by-default-devtools.timeline",
+            'blink.user_timing',
+            'devtools.timeline',
+            'disabled-by-default-devtools.timeline',
         ],
     })
 
     // Benchmark
-    console.log(await page.title())
+    log('BENCH', await page.title())
 
     // End Benchmark
     await sleep(40)
     await browser.stopTracing()
     await set_cpu_throttling(client, 1)
+
+    log('BENCH', 'Bench ended')
 
     // For mem tests
     // await client.send("Performance.enable");
@@ -140,8 +163,8 @@ async function main() {
     try {
         await page.close()
         await browser.close()
-    } catch (error) {
-        console.error("ERROR closing page", error)
+    } catch (e) {
+        error('BENCH', 'ERROR closing page: %o', e)
     }
 
     await server.stop(true)
